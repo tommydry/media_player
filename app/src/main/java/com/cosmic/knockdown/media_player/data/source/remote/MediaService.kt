d50ee6c5cb87bc6d4c31516778e1202e3a6bccbb
ArtSuh <artyom.suhov@gmail.com>
2020-03-06 18:41:11 +0300

    add new path to the remote content

--- app/src/main/java/com/cosmic/knockdown/media_player/data/source/remote/MediaService.kt
+++ app/src/main/java/com/cosmic/knockdown/media_player/data/source/remote/MediaService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface MediaService {
 
-    @GET("this_is_static/content.json")
+    @GET("media_player_content/content.json")
     suspend fun getAllAudio(): Media
 }
